1f04179d4bcfc4e113bda6307f57b1bcd73f5cda
ErtyPerty <115721409+ErtyPerty@users.noreply.github.com>
2023-02-28 13:11:36 +0000

    Update firstScreen.js

--- yandiya_app/screens/firstScreen.js
+++ yandiya_app/screens/firstScreen.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, TouchableOpacity, useRef } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -10,103 +9,109 @@ import {
   StatusBar,
   Image,
   TextInput,
-  setInput,
   Pressable,
   ImageBackground,
-  AppRegistry,
-  Platform,
-  input,
-  container,
   Dimensions,
-  Switch,
 } from 'react-native';
-  
-const styles = './styles.js';
 
-const image = './assets/yandiyaLogo_Wide.png';
-const icon = './assets/yandiyaLogo_Small.png';
-const bottom = './assets/bottom.jpg';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: windowWidth,
+    height: windowHeight,
+    resizeMode: 'cover',
+    position: 'absolute',
+    top: windowHeight * -0.2,
+    left: windowWidth * -0.05,
+  },
+  bottomImage: {
+    width: windowWidth,
+    height: windowHeight * 0.15,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  redBar: {
+    width: windowWidth * 0.95,
+    height: windowHeight * 0.03,
+    backgroundColor: '#e42c22',
+    borderRadius: 20,
+    position: 'absolute',
+    top: windowHeight * 0.05,
+    left: windowWidth * 0.025,
+  },
+  formContainer: {
+    height: windowHeight * 0.4,
+    width: windowWidth * 0.9,
+    backgroundColor: '#f8f7f7',
+    position: 'absolute',
+    top: windowHeight * 0.3,
+    left: windowWidth * 0.05,
+    borderRadius: 20,
+    boxShadow: '0px 0px 18px #252525',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  formTitle: {
+    color: 'black',
+    fontSize: 15,
+    position: 'absolute',
+    left: 25,
+    top: 90,
+    textAllign: 'center',
+    fontWeight: 'bold',
+  },
+  formSubtitle: {
+    color: 'black',
+    fontSize: 15,
+    position: 'absolute',
+    left: 25,
+    top: 120,
+    textAllign: 'center',
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    top: windowHeight * 0.37,
+    left: windowWidth * 0.33,
+    borderRadius: 40,
+    fontWeight: 'bold',
+  },
+});
 
 export function firstScreen({ navigation: { navigate } }) {
-   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
-    return (
-      <View style={styles.container}>
-        <ImageBackground source={image} style={styles.image} />
-        <ImageBackground
-          source={bottom}
-          style={{
-            width: 390,
-            height: 90,
-            position: 'absolute',
-            top: 670,
-            left: 0,
-          }}
-        />
-        <View
-          style={{
-            height: 30,
-            width: 370,
-            backgroundColor: '#e42c22',
-            borderRadius: 20,
-            position: 'absolute',
-            left: 10,
-            top: 50,
-          }}/>
-        <View
-          style={{
-            height: 520,
-            width: 330,
-            backgroundColor: '#f8f7f7',
-            border: 'black',
-            position: 'absolute',
-            left: 30,
-            top: 140,
-            borderRadius: 20,
-            boxShadow: '0px 0px 18px #252525',
-          }}>
-          <Text
-            style={{
-              color: 'black',
-              fontSize: 17,
-              position: 'absolute',
-              left: 15,
-              top: 90,
-              textAllign: 'center',
-              fontWeight: 'bold',
-            }}>
-            Installation and commission process
-          </Text>
-          <Text
-            style={{
-              color: 'black',
-              fontSize: 12,
-              position: 'absolute',
-              left: 20,
-              top: 160,
-              textAllign: 'center',
-              fontWeight: 'bold',
-            }}>
-            welcome to the yandiya simple commissions app
-          </Text>
-          <View
-            style={{
-              position: 'absolute',
-              left: 120,
-              top: 250,
-              fontWeight: 'bold',
-            }}>
-            <Button
-              color="green"
-              onPress={() => navigate('Welcome')}
-              title="Welcome"
-            />
-          </View>
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../assets/yandiyaLogo_Wide.png')}
+        style={styles.image}
+      />
+      <ImageBackground
+        source={require('../assets/bottom.jpg')}
+        style={styles.bottomImage}
+      />
+      <View style={styles.redBar} />
+
+      <View style={styles.formContainer}>
+        <Text style={styles.formTitle}>Installation and commissions</Text>
+        <Text style={styles.formSubtitle}>Digital technician form</Text>
+        <View style={styles.buttonContainer}>
+          <Button color="green" onPress={() => navigate('Welcome')} title="WELCOME" />
         </View>
       </View>
-    );
-  }
+    </View>
+  );
+}
