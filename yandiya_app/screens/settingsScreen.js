3e1881b3662e8cf1271370ca9c1ffcf5ccd8412f
marqueese <115721274+marqueese@users.noreply.github.com>
2023-03-01 15:07:50 +0000

    Update settingsScreen.js

--- yandiya_app/screens/settingsScreen.js
+++ yandiya_app/screens/settingsScreen.js
@@ -20,12 +20,13 @@ import {
   Switch,
   TouchableOpacity,
 } from 'react-native';
-import icon from '../assets/yandiyaLogo_Wide.png';
 
 const window = Dimensions.get('window');
-import ToggleSwitch from './toggleSwitch.js';
 const styles = './styles.js';
-const bottom = './assets/bottom.jpg';
+
+import image from '../assets/yandiyaLogo_Small.png';
+import icon from '../assets/yandiyaLogo_Wide.png';
+import bottom from '../assets/bottom.jpg';
 
 export function settingsScreen({ navigation: { navigate } }) {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -41,8 +42,11 @@ export function settingsScreen({ navigation: { navigate } }) {
           width: 390,
           height: 90,
           position: 'absolute',
-          top: 670,
+          top: 760,
           left: 0,
+          shadowColor: 'black',
+          shadowRadius: 14.65,
+          shadowOpacity: 5,
         }}
       />
       <View
@@ -59,35 +63,7 @@ export function settingsScreen({ navigation: { navigate } }) {
           shadowOpacity: 1,
         }}
       />
-      <TouchableOpacity onPress={() => navigate('main')}>
-        <ImageBackground
-        source={bottom}
-        style={{
-          width: 390,
-          height: 90,
-          position: 'absolute',
-          top: 760,
-          left: 0,
-          shadowColor: "black",
-          shadowRadius: 14.65,
-          shadowOpacity: 5,
-        }}
-      />
-      <View
-        style={{
-          height: 90,
-          width: 405,
-          backgroundColor: '#e42c22',
-          borderRadius: 20,
-          position: 'absolute',
-          left: -8,
-          top: 0,
-          shadowColor: "red",
-          shadowRadius: 14.65,
-          shadowOpacity: 1,
-        }}
-      />
-       <ImageBackground
+      <ImageBackground
         source={require('../assets/yandiyaLogo_Wide.png')}
         style={{
           width: window.width * 0.6,
@@ -97,22 +73,21 @@ export function settingsScreen({ navigation: { navigate } }) {
           left: window.width * 0.18,
         }}
       />
-      </TouchableOpacity>
       <View
         style={{
-          height: 520,
+          height: 180,
           width: 330,
           backgroundColor: '#f8f7f7',
           border: 'black',
           position: 'absolute',
           left: 30,
-          top: 140,
+          top: 240,
           borderRadius: 20,
           boxShadow: '0px 0px 18px #252525',
-          borderWidth: 5,
+          borderWidth: 2,
           borderColor: '#000000',
         }}>
-        <Button color="#212121" onPress={() => navigate('Info')} title="Info" />
+        <Button color="#212121" onPress={() => navigate('')} title="Info" />
         <Button
           color="#212121"
           onPress={() => navigate('LogOut')}
@@ -120,8 +95,8 @@ export function settingsScreen({ navigation: { navigate } }) {
         />
         <Button
           color="#212121"
-          onPress={() => navigate('comForm')}
-          title="form View"
+          onPress={() => navigate('')}
+          title="Help"
         />
         <View style={{ position: 'absolute', top: 0, right: 90 }}></View>
       </View>
