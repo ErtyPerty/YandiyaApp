3c5d70497ea5b580ded9d0ae4a1e630b8df7a756
marqueese <115721274+marqueese@users.noreply.github.com>
2023-03-01 15:14:56 +0000

    Update infoScreen.js

--- yandiya_app/screens/infoScreen.js
+++ yandiya_app/screens/infoScreen.js
@@ -119,18 +119,16 @@ export function infoScreen({ navigation: { navigate } }) {
           shadowOpacity: 1,
         }}
       />
-      <TouchableOpacity onPress={() => navigate('main')}>
-        <ImageBackground
-          source={icon}
-          style={{
-            width: window.width * 0.6,
-            height: window.height * 0.3,
-            position: 'absolute',
-            top: window.height * -0.04,
-            left: window.width * 0.18,
-          }}
-        />
-      </TouchableOpacity>
+      <ImageBackground
+        source={require('../assets/yandiyaLogo_Wide.png')}
+        style={{
+          width: window.width * 0.6,
+          height: window.height * 0.3,
+          position: 'absolute',
+          top: window.height * -0.04,
+          left: window.width * 0.18,
+        }}
+      />
       <View
         style={{
           height: 420,
